d24d42ce44bbcf7e440a501d4ec19e858b503ceb
chef-server <chef-server@example.com>
2012-03-10 19:21:53 +0100

    * update location key * add support for precise (must be changed for 0.44)

--- ceph/recipes/apt.rb
+++ ceph/recipes/apt.rb
@@ -1,6 +1,6 @@
 execute "add autobuild gpg key to apt" do
   command <<-EOH
-wget -q -O- https://raw.github.com/NewDreamNetwork/ceph/master/keys/release.asc \
+wget -q -O- https://raw.github.com/ceph/ceph/master/keys/release.asc \
 | sudo apt-key add -
   EOH
 end
@@ -10,9 +10,14 @@ template '/etc/apt/sources.list.d/ceph.list' do
   group 'root'
   mode '0644'
   source 'apt-sources-list.release.erb'
+  if node[:lsb][:codename] == "precise"
+    distribution = "oneiric"
+  else
+    distribution = node[:lsb][:codename]
+  end
   variables(
-    :codename => node[:lsb][:codename]
-  )
+    :codename => distribution
+    )
 end
 
 execute 'apt-get update'
