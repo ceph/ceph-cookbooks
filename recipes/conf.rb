628e33ead2fdcbe902612a172204bd1477a3fa85
Jesse Pretorius <jesse.pretorius@gmail.com>
2013-08-20 14:57:11 +0200

    Allow ceph::default to expect ceph-setup role, or direct config settings

--- recipes/conf.rb
+++ recipes/conf.rb
@@ -1,5 +1,20 @@
-raise "fsid must be set in config" if node["ceph"]["config"]['fsid'].nil?
-raise "mon_initial_members must be set in config" if node["ceph"]["config"]['mon_initial_members'].nil?
+if node['ceph']['config']['fsid'].nil? || node['ceph']['config']['mon_initial_members'].nil?
+  Log.debug("ceph-mds: Trying to retrieve fsid and mon from the ceph-setup role")
+  search_query = "roles:ceph-setup AND chef_environment:#{node.chef_environment}"
+  search_result = search(:node, search_query)
+  if search_result.length < 1
+    msg = "ceph-conf: The ceph fsid and mon_initial_members must be set in the config, or the ceph-setup role must be applied to a node."
+    Chef::Application.fatal! msg
+  end
+
+  fsid = search_result[0]['ceph']['config']['fsid']
+  Log.debug("ceph-mds: Found ceph fsid #{fsid} from the ceph-setup role")
+  node.set['ceph']['config']['fsid'] = fsid
+
+  mon_initial_members = search_result[0]['ceph']['config']['mon_initial_members']
+  Log.debug("ceph-mds: Found ceph mon_initial_members #{mon_initial_members} from the ceph-setup role")
+  node.set['ceph']['config']['mon_initial_members'] = mon_initial_members
+end
 
 mon_addresses = get_mon_addresses()
 
